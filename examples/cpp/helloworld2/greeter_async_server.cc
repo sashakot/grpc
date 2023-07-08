6efb25ff860eeda1dfe8827fec62cec8c3b2f492
Sasha Kotchubievsky <sashakot@nvidia.com>
2023-07-08 15:35:56 +0300

    Create Base class in for responder in Server

--- examples/cpp/helloworld2/greeter_async_server.cc
+++ examples/cpp/helloworld2/greeter_async_server.cc
@@ -46,6 +46,100 @@ using helloworld2::Greeter;
 using helloworld2::HelloReply;
 using helloworld2::HelloRequest;
 
+
+class CallDataBase {
+	public:
+		// Take in the "service" instance (in this case representing an asynchronous
+		// server) and the completion queue "cq" used for asynchronous communication
+		// with the gRPC runtime.
+		CallDataBase(Greeter::AsyncService* service, ServerCompletionQueue* cq)
+			: service_(service), cq_(cq),status_(CREATE), prefix_("Hello") {
+			}
+
+		virtual ~CallDataBase()
+		{
+		}
+
+		virtual void Proceed(bool = true) = 0;
+
+	protected:
+		// The means of communication with the gRPC runtime for an asynchronous
+		// server.
+		Greeter::AsyncService* service_;
+		// The producer-consumer queue where for asynchronous server notifications.
+		ServerCompletionQueue* cq_;
+		// Context for the rpc, allowing to tweak aspects of it such as the use
+		// of compression, authentication, as well as to send metadata back to the
+		// client.
+		ServerContext ctx_;
+
+		// What we get from the client.
+		HelloRequest request_;
+		// What we send back to the client.
+		HelloReply reply_;
+
+
+		// Let's implement a tiny state machine with the following states.
+		enum CallStatus { CREATE, PROCESS, FINISH };
+		CallStatus status_;  // The current serving state.
+
+		std::string prefix_;
+};
+
+class CallData: public CallDataBase
+{
+
+	// The means to get back to the client.
+	ServerAsyncResponseWriter<HelloReply> responder_;
+
+public:
+	  CallData(Greeter::AsyncService* service, ServerCompletionQueue* cq):
+		                  CallDataBase(service, cq), responder_(&ctx_)
+		{
+			// Invoke processing
+			Proceed();
+		}
+
+	  void Proceed(bool = true) override {
+		  if (status_ == CREATE) {
+			  // Make this instance progress to the PROCESS state.
+			  status_ = PROCESS;
+
+			  // As part of the initial CREATE state, we *request* that the system
+			  // start processing SayHello requests. In this request, "this" acts are
+			  // the tag uniquely identifying the request (so that different CallData
+			  // instances can serve different requests concurrently), in this case
+			  // the memory address of this CallData instance.
+
+			  std::cout << "New responder for 1:1 mode " << this << std::endl;
+
+			  service_->RequestSayHello(&ctx_, &request_, &responder_, cq_, cq_,
+					  this);
+		  } else if (status_ == PROCESS) {
+			  std::cout << "Create a new instance of responder to serve new client in 1:1 mode while this instance is busy with processing request message: " << request_.name()  << std::endl;
+			  // Spawn a new CallData instance to serve new clients while we process
+			  // the one for this CallData. The instance will deallocate itself as
+			  // part of its FINISH state.
+			  new CallData(service_, cq_);
+
+
+			  // The actual processing.
+			  reply_.set_message(prefix_ + request_.name());
+
+			  // And we are done! Let the gRPC runtime know we've finished, using the
+			  // memory address of this instance as the uniquely identifying tag for
+			  // the event.
+			  status_ = FINISH;
+			  responder_.Finish(reply_, Status::OK, this);
+		  } else {
+			  GPR_ASSERT(status_ == FINISH);
+			  std::cout << "Delete responder for 1:1 mode " << this << std::endl;
+			  // Once in the FINISH state, deallocate ourselves (CallData).
+			  delete this;
+		  }
+	  }
+};
+
 class ServerImpl final {
  public:
   ~ServerImpl() {
@@ -77,74 +171,6 @@ class ServerImpl final {
 
  private:
   // Class encompasing the state and logic needed to serve a request.
-  class CallData {
-   public:
-    // Take in the "service" instance (in this case representing an asynchronous
-    // server) and the completion queue "cq" used for asynchronous communication
-    // with the gRPC runtime.
-    CallData(Greeter::AsyncService* service, ServerCompletionQueue* cq)
-        : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE) {
-      // Invoke the serving logic right away.
-      Proceed();
-    }
-
-    void Proceed() {
-      if (status_ == CREATE) {
-        // Make this instance progress to the PROCESS state.
-        status_ = PROCESS;
-
-        // As part of the initial CREATE state, we *request* that the system
-        // start processing SayHello requests. In this request, "this" acts are
-        // the tag uniquely identifying the request (so that different CallData
-        // instances can serve different requests concurrently), in this case
-        // the memory address of this CallData instance.
-        service_->RequestSayHello(&ctx_, &request_, &responder_, cq_, cq_,
-                                  this);
-      } else if (status_ == PROCESS) {
-        // Spawn a new CallData instance to serve new clients while we process
-        // the one for this CallData. The instance will deallocate itself as
-        // part of its FINISH state.
-        new CallData(service_, cq_);
-
-        // The actual processing.
-        std::string prefix("Hello ");
-        reply_.set_message(prefix + request_.name());
-
-        // And we are done! Let the gRPC runtime know we've finished, using the
-        // memory address of this instance as the uniquely identifying tag for
-        // the event.
-        status_ = FINISH;
-        responder_.Finish(reply_, Status::OK, this);
-      } else {
-        GPR_ASSERT(status_ == FINISH);
-        // Once in the FINISH state, deallocate ourselves (CallData).
-        delete this;
-      }
-    }
-
-   private:
-    // The means of communication with the gRPC runtime for an asynchronous
-    // server.
-    Greeter::AsyncService* service_;
-    // The producer-consumer queue where for asynchronous server notifications.
-    ServerCompletionQueue* cq_;
-    // Context for the rpc, allowing to tweak aspects of it such as the use
-    // of compression, authentication, as well as to send metadata back to the
-    // client.
-    ServerContext ctx_;
-
-    // What we get from the client.
-    HelloRequest request_;
-    // What we send back to the client.
-    HelloReply reply_;
-
-    // The means to get back to the client.
-    ServerAsyncResponseWriter<HelloReply> responder_;
-
-    // Let's implement a tiny state machine with the following states.
-    enum CallStatus { CREATE, PROCESS, FINISH };
-    CallStatus status_;  // The current serving state.
-  };
 
   // This can be run in multiple threads if needed.
   void HandleRpcs() {
